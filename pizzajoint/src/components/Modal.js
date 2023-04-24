b425a0ef4b51c8d59bdfbc571e716b26c3ed70b4
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-24 23:40:25 +0530

    add css to modal

--- pizzajoint/src/components/Modal.js
+++ pizzajoint/src/components/Modal.js
@@ -20,7 +20,14 @@ const Modal = ({ showModel, setShowModel }) => {
           variants={backdrop}
           initial="hidden"
           animate="visible"
-        ></motion.div>
+        >
+          <motion.div className="modal">
+            <p>want to make another pizza ?</p>
+            <Link to="/">
+              <button>Start Again</button>
+            </Link>
+          </motion.div>
+        </motion.div>
       )}
     </AnimatePresence>
   );
